c02b532cbc69a070abbac2ea31de070e1574d3ed
Sameer <sameersingh192004@gmail.com>
2024-08-26 02:14:05 +0530

    image fix

--- src/_auth/authLayout.tsx
+++ src/_auth/authLayout.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from 'react-router-dom'
-
+import sideimg from '../../public/assets/images/side-img.svg'
 
 const AuthLayout = () => {
   const isAuthenticated = false
@@ -12,7 +12,7 @@ const AuthLayout = () => {
       <section className='flex flex-1 justify-center items-center flex-col py-10'>
         <Outlet/>
       </section>
-      <img src="public\assets\images\side-img.svg"
+      <img src={sideimg}
        alt="logo"
        className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'
        />
